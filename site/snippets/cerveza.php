5f1cb1d191d3adcc0224bd0bca8161ab697cd45f
Adam Parker <adam@theadamparker.com>
2019-04-10 18:33:57 -0400

    CSS grid

--- site/snippets/cerveza.php
+++ site/snippets/cerveza.php
@@ -1,11 +1,25 @@
-<section id="cerveza-de-verdad">
-  <h1><?= $data->title() ?></h1>
-  <?= $data->text()->kirbytext() ?>
-
+<section id="cerveza-de-verdad" class="section--grid">
   <?php if($image = $data->image()): ?>
+  <figure class="grid-3-6 photoModule">
     <img src="<?= $image->url() ?>" alt="" />
-    <?= $image->url() ?>
+  </figure>
   <?php endif ?>
+
+  <div class="textWrap grid-3-6">
+    <h1><?= $data->title() ?></h1>
+    <?= $data->text()->kirbytext() ?>
+  </div>
+
+  <div class="grid-2-6">
+    PHOTO
+  </div>
+  <div class="grid-2-6">
+    PHOTO
+  </div>
+  <div class="grid-2-6">
+    PHOTO
+  </div>
+
 
   <ul>
     <?php foreach($data->children()->listed() as $product): ?>
