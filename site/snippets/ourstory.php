103bb4117eb72804082283ef21c3dcfd8ad5a326
Adam Parker <adam@theadamparker.com>
2019-04-10 17:43:26 -0400

    Building

--- site/snippets/ourstory.php
+++ site/snippets/ourstory.php
@@ -1,7 +1,10 @@
 <section id="our-story" class="section--photoBG" style="background-image: url('<?php if($image = $data->image()): ?>
   <?= $image->url() ?>
 <?php endif ?>')">
-  <h1><?= $data->title() ?></h1>
-  <?= $data->text()->kirbytext() ?>
+  <div class="textWrap textWrap--fixed">
+    <h1><?= $data->title() ?></h1>
+    <?= $data->text()->kirbytext() ?>
+  </div>
+
 
 </section>
