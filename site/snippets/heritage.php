9e47d33b5434e47b750ba7d57233fa706db7a8a3
Adam Parker <adam@theadamparker.com>
2019-04-11 08:25:24 -0400

    Styling

--- site/snippets/heritage.php
+++ site/snippets/heritage.php
@@ -1,4 +1,45 @@
 <section id="our-heritage">
-  <h1><?= $data->title() ?></h1>
-  <?= $data->text()->kirbytext() ?>
+  <div class="section--grid">
+    <?php if($image = $data->image()): ?>
+    <figure class="col-3-6 row-2 photoModule">
+      <img src="<?= $image->url() ?>" alt="" />
+    </figure>
+    <?php endif ?>
+
+    <div class="textWrap col-3-6 row-2">
+      <h1><?= $data->title() ?></h1>
+      <?= $data->text()->kirbytext() ?>
+    </div>
+
+    <div class="col-2-6">
+      PHOTO
+    </div>
+    <div class="col-2-6">
+      PHOTO
+    </div>
+    <div class="col-2-6">
+      PHOTO
+    </div>
+
+    <div class="textWrap col-3-6 row-2">
+      Text
+    </div>
+
+    <?php if($image = $data->image()): ?>
+    <figure class="col-3-6 row-2 photoModule">
+      <img src="<?= $image->url() ?>" alt="" />
+    </figure>
+    <?php endif ?>
+
+    <div class="col-2-6">
+      PHOTO
+    </div>
+    <div class="col-2-6">
+      PHOTO
+    </div>
+    <div class="col-2-6">
+      PHOTO
+    </div>
+
+  </div>
 </section>
