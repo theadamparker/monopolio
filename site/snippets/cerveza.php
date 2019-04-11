3ffec4c270bf324b19e92b42e81e4566d123a605
Adam Parker <adam@theadamparker.com>
2019-04-10 22:03:07 -0400

    Added CSS grid structure

--- site/snippets/cerveza.php
+++ site/snippets/cerveza.php
@@ -1,29 +1,55 @@
-<section id="cerveza-de-verdad" class="section--grid">
-  <?php if($image = $data->image()): ?>
-  <figure class="grid-3-6 photoModule">
-    <img src="<?= $image->url() ?>" alt="" />
-  </figure>
-  <?php endif ?>
+<section id="cerveza-de-verdad" class="">
+  <div class="section--grid">
+    <?php if($image = $data->image()): ?>
+    <figure class="col-3-6 row-2 photoModule">
+      <img src="<?= $image->url() ?>" alt="" />
+    </figure>
+    <?php endif ?>
 
-  <div class="textWrap grid-3-6">
-    <h1><?= $data->title() ?></h1>
-    <?= $data->text()->kirbytext() ?>
+    <div class="textWrap col-3-6 row-2">
+      <h1><?= $data->title() ?></h1>
+      <?= $data->text()->kirbytext() ?>
+    </div>
+
+    <div class="col-2-6">
+      PHOTO
+    </div>
+    <div class="col-2-6">
+      PHOTO
+    </div>
+    <div class="col-2-6">
+      PHOTO
+    </div>
+
+    <div class="textWrap col-3-6 row-2">
+      <?= $data->text()->kirbytext() ?>
+    </div>
+
+    <?php if($image = $data->image()): ?>
+    <figure class="col-3-6 row-2 photoModule">
+      <img src="<?= $image->url() ?>" alt="" />
+    </figure>
+    <?php endif ?>
+
+    <div class="col-2-6">
+      PHOTO
+    </div>
+    <div class="col-2-6">
+      PHOTO
+    </div>
+    <div class="col-2-6">
+      PHOTO
+    </div>
+
   </div>
 
-  <div class="grid-2-6">
-    PHOTO
-  </div>
-  <div class="grid-2-6">
-    PHOTO
-  </div>
-  <div class="grid-2-6">
-    PHOTO
-  </div>
+  <div class="products">
 
-
-  <ul>
     <?php foreach($data->children()->listed() as $product): ?>
-      <?= $product->title() ?>
+      <div class="product">
+        <?= $product->title() ?>
+      </div>
     <?php endforeach ?>
-  </ul>
+
+  </div>
 </section>
