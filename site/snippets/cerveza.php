3ff563ff8d1a4838854140155d3d8f58f2ea262f
Adam Parker <adam@theadamparker.com>
2019-04-11 22:44:08 -0400

    Panel updates

--- site/snippets/cerveza.php
+++ site/snippets/cerveza.php
@@ -1,14 +1,15 @@
 <section id="cerveza-de-verdad" class="">
   <div class="section--grid">
-    <?php if($image = $data->image()): ?>
+    <?php if($image = $data->cover()): ?>
     <figure class="col-3-6 row-2 photoModule">
       <img src="<?= $image->url() ?>" alt="" />
+      <?= $image->url() ?>
     </figure>
     <?php endif ?>
 
     <div class="textWrap col-3-6 row-2">
-      <h1><?= $data->title() ?></h1>
-      <?= $data->text()->kirbytext() ?>
+      <h1><?= $data->headline() ?></h1>
+      <?= $data->text1()->kirbytext() ?>
     </div>
 
     <?php foreach($data->images() as $image): ?>
@@ -21,7 +22,7 @@
       <?= $data->text2()->kirbytext() ?>
     </div>
 
-    <?php if($image = $data->image()): ?>
+    <?php if($image = $data->cover2()): ?>
     <figure class="col-3-6 row-2 photoModule">
       <img src="<?= $image->url() ?>" alt="" />
     </figure>
@@ -48,9 +49,9 @@
           <img src="<?= $image->url() ?>" alt="" />
         </figure>
         <?php endif ?>
-        <h2><?= $product->title() ?></h2>
-        <?= $product->text()->kirbytext() ?>
-        <?= $product->hopVariety()->kirbytext() ?>
+        <h2><?= $product->name() ?></h2>
+        <?= $product->description()->kirbytext() ?>
+        <?= $product->stats()->hops()->kirbytext() ?>
         <?= $product->ABV()->kirbytext() ?>
         <?= $product->IBU()->kirbytext() ?>
       </div>
