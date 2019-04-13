7f45bc279ec8635fcd6c91c11cff64eaaf630b06
Adam Parker <adam@theadamparker.com>
2019-04-12 21:59:29 -0400

    Added additional heritage photos

--- site/snippets/heritage.php
+++ site/snippets/heritage.php
@@ -50,5 +50,60 @@
     </figure>
     <?php endforeach ?>
 
+    <?php if($image = $data->cover3()->toFile()): ?>
+    <figure class="col-3-6 row-2 photoModule">
+      <img
+      src="<?= $image->url()?>"
+      srcset="<?= $image->srcset('featuredPhoto') ?>"
+      alt="<?= $image->alt()?>"
+      />
+    </figure>
+    <?php endif ?>
+
+    <div class="textWrap col-3-6 row-2">
+      <h1><?= $data->headline3() ?></h1>
+      <?= $data->text3()->kirbytext() ?>
+    </div>
+
+    <?php foreach($data->gallery3()->toFiles() as $image): ?>
+    <figure class="col-2-6">
+      <img
+      src="<?= $image->url()?>"
+      srcset="<?= $image->srcset('galleryPhoto') ?>"
+      alt="<?= $image->alt()?>"
+      />
+    </figure>
+    <?php endforeach ?>
+
+    <div class="textWrap col-3-6 row-2">
+      <h1><?= $data->headline4() ?></h1>
+      <?= $data->text4()->kirbytext() ?>
+    </div>
+
+    <?php if($image = $data->cover4()->toFile()): ?>
+    <figure class="col-3-6 row-2 photoModule">
+      <img
+      src="<?= $image->url()?>"
+      srcset="<?= $image->srcset('featuredPhoto') ?>"
+      alt="<?= $image->alt()?>"
+      />
+    </figure>
+    <?php endif ?>
+
+    <?php if($image = $data->cover5()->toFile()): ?>
+    <figure class="col-3-6 row-2 photoModule">
+      <img
+      src="<?= $image->url()?>"
+      srcset="<?= $image->srcset('featuredPhoto') ?>"
+      alt="<?= $image->alt()?>"
+      />
+    </figure>
+    <?php endif ?>
+
+    <div class="textWrap col-3-6 row-2">
+      <h1><?= $data->headline5() ?></h1>
+      <?= $data->text5()->kirbytext() ?>
+    </div>
+
   </div>
 </section>
