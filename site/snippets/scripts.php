ab63bf9b192f5036c439df452f172a5cac243f44
Adam Parker <adam@theadamparker.com>
2019-04-16 00:35:39 -0400

    Deleted extraneous CSS templates

--- site/snippets/scripts.php
+++ site/snippets/scripts.php
@@ -2,6 +2,5 @@
   src="https://code.jquery.com/jquery-2.2.4.min.js"
   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
   crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
 <?php echo js('assets/js/build/global.min.js') ?>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDoIvLsfzYmLQKGrNs0XxF_MCRBj61kG7I&callback=initMap" async defer></script>
