7059a0e5c3ce6be750ddb612f4b05b416cb5611d
Adam Parker <adam@theadamparker.com>
2019-04-20 13:25:19 -0400

    Fixed overflow on homephoto

--- site/snippets/homephoto.php
+++ site/snippets/homephoto.php
@@ -1,9 +1,2 @@
-<section class="homephoto section--fullBleed">
-  <?php if($image = $page->cover()->toFile()): ?>
-    <img
-    src="<?= $image->url()?>"
-    srcset="<?= $image->srcset() ?>"
-    alt="<?= $image->alt()?>"
-    />
-  <?php endif ?>
+<section class="homephoto"<?php if($image = $page->cover()->toFile()): ?> style="background-image: url('<?= $image->url()?>');"<?php endif ?>>
 </section>
